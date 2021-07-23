bf633ce9c50ff4f9eedde53d7858fb9626dba42b
ammshahin <60013707+ammshahin@users.noreply.github.com>
2021-07-23 21:03:04 +0600

    duplicacy check

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import { useEffect, React, useState } from 'react';
+import React  from 'react';
+import { useEffect,  useState } from 'react';
 import './App.css';
 import Person from './components/Persons/Person';
 
 function App() {
   const [users, setUsers] = useState([]);
   const [team, setTeam] = useState([]);
+  
 
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=10')
@@ -13,9 +15,19 @@ function App() {
       .then(data => setUsers(data.results))
   }, []);
 
+  // const handleAddTeam = (props) => setTeam([...team, props]);
   const handleAddTeam = (props) => setTeam([...team, props]);
-
   
+  checkDuplicate();
+  function checkDuplicate() {
+      let result = false;
+     
+      result = team.some((element, index) => {return team.indexOf(element) !== index});
+      if(result) {
+         window.alert('Person is already added in Team!')
+         team.pop();
+      } 
+   }
   return (
     <div className="App">
       <div className="userList">
@@ -27,7 +39,7 @@ function App() {
         <h1>My Team</h1>
         <ul>
           {
-            team.map(team => <li>{team.name.title+" "+team.name.first}</li> )
+            team.map(team => <li>{team.name.title + " " + team.name.first}</li>)
           }
         </ul>
 
